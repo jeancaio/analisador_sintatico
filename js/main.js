8b15e3767486b2956f42b67e5266a3eb1f54f56a
jeancaio <jeanandreolla@hotmail.com>
2018-06-12 17:30:17 -0300

    primeira linha

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ $('.btn-detalhes').click(function() {
     $(this).find('span').text('Ocultar   detalhes');
   }
 })
+
+var click = 0
+$('.botao-passos').click(function() {
+  click += 1
+  if (click <= 1) {
+    $('.tbody-sintatico').append("<tr class='tr-sintatico'></tr>")
+    $('.tr-sintatico').append("<td>$S</td>")
+    $('.tr-sintatico').append("<td>" + $('.token').val() + "$</td>")
+    var cedula = $(".tabela-automato").find('.linha-s').find(".coluna-"+$('.token').val().split('')[0]).text()
+    $('.tr-sintatico').append("<td class='td-acao'>" + cedula + "</td>")
+  }
+  testaGramatica();
+})
+
+function testaGramatica() {
+}
