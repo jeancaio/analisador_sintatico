393ab4cfeab5a39506bbbd44aae2591f1be7eed4
jeancaio <jeanandreolla@hotmail.com>
2018-06-13 19:25:02 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $('.btn-detalhes').click(function() {
   $('.area-detalhes').toggleClass('hidden')
-  if ($(this).find('span').text() === 'Ocular detalhes') {
+  if ($(this).find('span').text() === 'Ocultar detalhes') {
     $(this).find('span').text('Mostrar detalhes');
   } else {
-    $(this).find('span').text('Ocultar   detalhes');
+    $(this).find('span').text('Ocultar detalhes');
   }
 })
 
