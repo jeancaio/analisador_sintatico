1df59c926bd831d0e328ad6607f6806c0659a891
jeancaio <jeanandreolla@hotmail.com>
2018-06-13 20:48:39 -0300

    refatoração/função compara primeira e ultima letra

--- js/main.js
+++ js/main.js
@@ -8,17 +8,49 @@ $('.btn-detalhes').click(function() {
 })
 
 var click = 0
+var $tabela = $('.tbody-sintatico')
+
 $('.botao-passos').click(function() {
   click += 1
   if (click <= 1) {
-    $('.tbody-sintatico').append("<tr class='tr-sintatico'></tr>")
-    $('.tr-sintatico').append("<td>$S</td>")
-    $('.tr-sintatico').append("<td>" + $('.token').val() + "$</td>")
+    $tabela.append(trSintatico())
+    $('.tr-sintatico').append(tdPilha("$S"))
+    $('.tr-sintatico').append(tdEntrada(($('.token').val()) + "$"))
     var cedula = $(".tabela-automato").find('.linha-s').find(".coluna-"+$('.token').val().split('')[0]).text()
-    $('.tr-sintatico').append("<td class='td-acao'>" + cedula + "</td>")
+    $('.tr-sintatico').append(tdAcao(cedula ))
   }
   testaGramatica();
 })
 
 function testaGramatica() {
+  var ultima_letra_pilha =  $('.td-pilha').last().text().split('').pop()
+  var primeira_letra_entrada = $('.td-entrada').last().text().split('')[0]
+
+  if (primeira_letra_entrada === ultima_letra_pilha) {
+    var texto_pilha = $('.td-pilha').last().text()
+    $tabela.append(trSintatico())
+    $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0,(texto_pilha.length - 1))))
+    $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text().substr(1)))
+    $('.tr-sintatico').last().append(tdAcao("Lê "+ ultima_letra_pilha + ' e desempilha'))
+  }
+}
+
+function cedulaComparavel(linha, coluna) {
+  return cedula = $(".tabela-automato").find('.linha-s').find(".coluna-"+$('.token').val().split('')[0]).text()
+}
+
+function tdPilha(elemento) {
+  return "<td class='td-pilha'>" + elemento + "</td>"
+}
+
+function tdEntrada(elemento) {
+  return "<td class='td-entrada'>" + elemento + "</td>"
+}
+
+function tdAcao(elemento) {
+  return "<td class='td-acao'>" + elemento + "</td>"
+}
+
+function trSintatico() {
+  return "<tr class='tr-sintatico'></tr>"
 }
