3b09f71a689518055e79de44a7ae328a39396e52
jeancaio <jeanandreolla@hotmail.com>
2018-06-15 19:57:33 -0300

    continuação...

--- js/main.js
+++ js/main.js
@@ -16,27 +16,61 @@ $('.botao-passos').click(function() {
     $tabela.append(trSintatico())
     $('.tr-sintatico').append(tdPilha("$S"))
     $('.tr-sintatico').append(tdEntrada(($('.token').val()) + "$"))
-    var cedula = $(".tabela-automato").find('.linha-s').find(".coluna-"+$('.token').val().split('')[0]).text()
-    $('.tr-sintatico').append(tdAcao(cedula ))
+    var cedula = $(".tabela-automato").find('.linha-s').find(".coluna-" + $('.token').val().split('')[0]).text()
+    $('.tr-sintatico').append(tdAcao(cedula))
+    testaGramatica();
   }
-  testaGramatica();
 })
 
 function testaGramatica() {
-  var ultima_letra_pilha =  $('.td-pilha').last().text().split('').pop()
+  var ultima_letra_pilha = $('.td-pilha').last().text().split('').pop()
   var primeira_letra_entrada = $('.td-entrada').last().text().split('')[0]
 
   if (primeira_letra_entrada === ultima_letra_pilha) {
     var texto_pilha = $('.td-pilha').last().text()
+
+    $('.tr-sintatico').last().append(tdAcao("Lê " + ultima_letra_pilha + ' e desempilha'))
     $tabela.append(trSintatico())
-    $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0,(texto_pilha.length - 1))))
+    $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0, (texto_pilha.length - 1))))
     $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text().substr(1)))
-    $('.tr-sintatico').last().append(tdAcao("Lê "+ ultima_letra_pilha + ' e desempilha'))
+
+    var texto_entrada = $('.td-entrada').last().text();
+    cedulaComparavel($('.td-pilha').last().text().split('').pop().toLowerCase(), texto_entrada.split('')[0])
+  } else {
+    $('.botao-passos').click(function() {
+      empilha();
+    });
+  }
+
+  debugger
+  if ($('.td-acao').last().text() === '') {
+    var ultima_seq = $('.td-acao').last().text().split('> ')[1].split('').reverse().join('')
+    var texto_pilha = $('.td-pilha').last().text()
+
+    $tabela.append(trSintatico())
+    $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0, (texto_pilha.length - 1)) + ultima_seq))
+    $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text()))
   }
 }
 
+function empilha() {
+  var ultima_seq = $('.td-acao').last().text().split('> ')[1].split('').reverse().join('')
+  var texto_pilha = $('.td-pilha').last().text()
+
+  $tabela.append(trSintatico())
+  $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0, (texto_pilha.length - 1)) + ultima_seq))
+  $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text()))
+  testaGramatica();
+}
+
 function cedulaComparavel(linha, coluna) {
-  return cedula = $(".tabela-automato").find('.linha-s').find(".coluna-"+$('.token').val().split('')[0]).text()
+  var cedula = $(".tabela-automato").find('.linha-' + linha).find(".coluna-" + coluna).text()
+  if (cedula != '') {
+    return $('.tr-sintatico').last().append(tdAcao(cedula))
+  } else {
+    return $('.tr-sintatico').last().append(tdAcao("Erro em x iterações"))
+  }
+
 }
 
 function tdPilha(elemento) {
