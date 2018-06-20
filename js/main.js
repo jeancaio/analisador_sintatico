78516b263f7337be910acfdae95bc7cb7020ce99
jeancaio <jeanandreolla@hotmail.com>
2018-06-20 20:37:09 -0300

    funcionou

--- js/main.js
+++ js/main.js
@@ -16,17 +16,48 @@ $('.botao-passos').click(function() {
     $tabela.append(trSintatico())
     $('.tr-sintatico').append(tdPilha("$S"))
     $('.tr-sintatico').append(tdEntrada(($('.token').val()) + "$"))
-    var cedula = $(".tabela-automato").find('.linha-s').find(".coluna-" + $('.token').val().split('')[0]).text()
+    var cedula = $(".tabela-automato").find('.linha-S').find(".coluna-" + $('.token').val().split('')[0]).text()
     $('.tr-sintatico').append(tdAcao(cedula))
-    testaGramatica();
   }
+  empilha();
 })
+  //
+  // if ($('td').last()[0].className === "td-entrada") {
+  //   var texto_entrada = $('.td-entrada').last().text();
+  //   cedulaComparavel($('.td-pilha').last().text().split('').pop(), texto_entrada.split('')[0])
+  // }
 
-function testaGramatica() {
-  var ultima_letra_pilha = $('.td-pilha').last().text().split('').pop()
-  var primeira_letra_entrada = $('.td-entrada').last().text().split('')[0]
+function empilha() {
+  var ultima_seq = $('.td-acao').last().text().split('> ')[1].split('').reverse().join('')
+  var texto_pilha = $('.td-pilha').last().text()
 
-  if (primeira_letra_entrada === ultima_letra_pilha) {
+  if ($('.td-acao').last().text().split('> ')[1] === '€') {
+    $tabela.append(trSintatico())
+    $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0, (texto_pilha.length - 1))))
+    $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text()))
+  } else {
+    $tabela.append(trSintatico())
+    $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0, (texto_pilha.length - 1)) + ultima_seq))
+    $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text()))
+  }
+
+  var texto_entrada = $('.td-entrada').last().text();
+  cedulaComparavel($('.td-pilha').last().text().split('').pop(), texto_entrada.split('')[0])
+}
+
+function cedulaComparavel(linha, coluna) {
+  if (coluna == '$') {
+    coluna = 's'
+  }
+  if (linha == '$') {
+    linha = 'S'
+  }
+  if (linha === "S" && coluna === "s")  {
+    return $('.tr-sintatico').last().append(tdAcao("OK em " + $('.tbody-sintatico').find('tr').length + " iterações"))
+  } else if (coluna === linha) {
+    var ultima_letra_pilha = $('.td-pilha').last().text().split('').pop()
+    var primeira_letra_entrada = $('.td-entrada').last().text().split('')[0]
+
     var texto_pilha = $('.td-pilha').last().text()
 
     $('.tr-sintatico').last().append(tdAcao("Lê " + ultima_letra_pilha + ' e desempilha'))
@@ -35,40 +66,15 @@ function testaGramatica() {
     $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text().substr(1)))
 
     var texto_entrada = $('.td-entrada').last().text();
-    cedulaComparavel($('.td-pilha').last().text().split('').pop().toLowerCase(), texto_entrada.split('')[0])
-  } else {
-    $('.botao-passos').click(function() {
-      empilha();
-    });
+    cedulaComparavel($('.td-pilha').last().text().split('').pop(), texto_entrada.split('')[0])
+  }  else {
+    var cedula = $(".tabela-automato").find('.linha-' + linha).find(".coluna-" + coluna).text()
+    if (cedula != '') {
+      return $('.tr-sintatico').last().append(tdAcao(cedula))
+    } else {
+      return $('.tr-sintatico').last().append(tdAcao("Erro em " + $('.tbody-sintatico').find('tr').length + " iterações"))
+    }
   }
-
-  if ($('td').last()[0].className === "td-entrada") {
-    var texto_entrada = $('.td-entrada').last().text();
-    cedulaComparavel($('.td-pilha').last().text().split('').pop().toLowerCase(), texto_entrada.split('')[0])
-  }
-}
-
-function empilha() {
-  var ultima_seq = $('.td-acao').last().text().split('> ')[1].split('').reverse().join('')
-  var texto_pilha = $('.td-pilha').last().text()
-
-  $tabela.append(trSintatico())
-  $('.tr-sintatico').last().append(tdPilha(texto_pilha.substr(0, (texto_pilha.length - 1)) + ultima_seq))
-  $('.tr-sintatico').last().append(tdEntrada($('.td-entrada').last().text()))
-  testaGramatica();
-}
-
-function cedulaComparavel(linha, coluna) {
-  if (coluna == '$') {
-    coluna = 's'
-  }
-  var cedula = $(".tabela-automato").find('.linha-' + linha).find(".coluna-" + coluna).text()
-  if (cedula != '') {
-    return $('.tr-sintatico').last().append(tdAcao(cedula))
-  } else {
-    return $('.tr-sintatico').last().append(tdAcao("Erro em " + $('.tbody-sintatico').find('tr').length + " iterações"))
-  }
-
 }
 
 function tdPilha(elemento) {
